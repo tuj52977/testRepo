11289f80ec1b37dc230d4cc0fe39736c08a5d23c
Maxwell ODonnell <tuj52977@temple.edu>
2021-09-01 15:06:39 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args)
+    throws IOException{
+        BufferedReader reader = new BufferedReader(
+        new InputStreamReader(System.in));
+        System.out.println("How many days?");
+        int days = Integer.parseInt(reader.readLine());
+        System.out.println("Days "+days);
+            while(days != -1){
+                    
+                }
+
+    }
+}
